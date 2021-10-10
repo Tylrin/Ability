e891738ec1c88e0ecf181cc1e51dbe7c4cc33d13
Tylrin <t.gellien@outlook.com>
2021-10-10 20:50:06 +0200

    Add helper classes

--- Source/Ability/Private/AbilitySystem/GameplayAbilitySystemComponent.cpp
+++ Source/Ability/Private/AbilitySystem/GameplayAbilitySystemComponent.cpp
@@ -3,3 +3,112 @@
 
 #include "AbilitySystem/GameplayAbilitySystemComponent.h"
 
+void UGameplayAbilitySystemComponent::InitAbilityActorInfo(AActor* InOwnerActor, AActor* InAvatarActor)
+{
+	Super::InitAbilityActorInfo(InOwnerActor, InAvatarActor);
+
+	if (AbilityActorInfo)
+	{
+		if (UGameInstance* GameInstance = InOwnerActor->GetGameInstance())
+		{
+			// Sign up for possess/unpossess events so that we can update the cached AbilityActorInfo accordingly
+			GameInstance->GetOnPawnControllerChanged().AddDynamic(this, &UGameplayAbilitySystemComponent::OnPawnControllerChanged);
+		}
+	}
+
+	GrantDefaultAbilitiesAndAttributes();
+}
+
+void UGameplayAbilitySystemComponent::BeginDestroy()
+{
+	if (AbilityActorInfo && AbilityActorInfo->OwnerActor.IsValid())
+	{
+		if (UGameInstance* GameInstance = AbilityActorInfo->OwnerActor->GetGameInstance())
+		{
+			GameInstance->GetOnPawnControllerChanged().RemoveAll(this);
+		}
+	}
+
+	Super::BeginDestroy();
+}
+
+FGameplayAbilitySpecHandle UGameplayAbilitySystemComponent::GrantAbilityOfType(TSubclassOf<UGameplayAbility> AbilityType, bool bRemoveAfterActivation)
+{
+	FGameplayAbilitySpecHandle AbilityHandle;
+	if (AbilityType)
+	{
+		FGameplayAbilitySpec AbilitySpec(AbilityType);
+		AbilitySpec.RemoveAfterActivation = bRemoveAfterActivation;
+
+		AbilityHandle = GiveAbility(AbilitySpec);
+	}
+	return AbilityHandle;
+}
+
+void UGameplayAbilitySystemComponent::RemoveAbilityOfType(TSubclassOf<UGameplayAbility> AbilityType)
+{
+	FGameplayAbilitySpecHandle AbilityHandle;
+	if (AbilityType)
+	{
+		ClearAbility(AbilityHandle);
+	}
+}
+
+void ClearAbility(const FGameplayAbilitySpecHandle& Handle);
+
+void UGameplayAbilitySystemComponent::GrantDefaultAbilitiesAndAttributes()
+{
+	// Reset/Remove abilities if we had already added them
+	{
+		//for (UAttributeSet* AttribSetInstance : AddedAttributes)
+		//{
+		//	GetSpawnedAttributes_Mutable().Remove(AttribSetInstance);
+		//}
+
+		for (FGameplayAbilitySpecHandle AbilityHandle : DefaultAbilityHandles)
+		{
+			SetRemoveAbilityOnEnd(AbilityHandle);
+		}
+
+		//AddedAttributes.Empty(DefaultAttributes.Num());
+		DefaultAbilityHandles.Empty(DefaultAbilities.Num());
+	}
+
+	// Default abilities
+	{
+		DefaultAbilityHandles.Reserve(DefaultAbilities.Num());
+		for (const TSubclassOf<UGameplayAbility>& Ability : DefaultAbilities)
+		{
+			if (*Ability)
+			{
+				DefaultAbilityHandles.Add(GiveAbility(FGameplayAbilitySpec(Ability)));
+			}
+		}
+	}
+
+	// Default attributes
+	//{
+	//	for (const FGameplayAttributeApplication& Attributes : DefaultAttributes)
+	//	{
+	//		if (Attributes.AttributeSetType)
+	//		{
+	//			UAttributeSet* NewAttribSet = NewObject<UAttributeSet>(this, Attributes.AttributeSetType);
+	//			if (Attributes.InitializationData)
+	//			{
+	//				NewAttribSet->InitFromMetaDataTable(Attributes.InitializationData);
+	//			}
+	//			AddedAttributes.Add(NewAttribSet);
+	//			AddAttributeSetSubobject(NewAttribSet);
+	//		}
+	//	}
+	//}
+}
+
+void UGameplayAbilitySystemComponent::OnPawnControllerChanged(APawn* Pawn, AController* NewController)
+{
+	if (AbilityActorInfo && AbilityActorInfo->OwnerActor == Pawn && AbilityActorInfo->PlayerController != NewController)
+	{
+		// Reinit the cached ability actor info (specifically the player controller)
+		AbilityActorInfo->InitFromActor(AbilityActorInfo->OwnerActor.Get(), AbilityActorInfo->AvatarActor.Get(), this);
+	}
+}
--- Source/Ability/Public/AbilitySystem/GameplayAbilitySystemComponent.h
+++ Source/Ability/Public/AbilitySystem/GameplayAbilitySystemComponent.h
@@ -2,16 +2,65 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
+#include "Delegates/DelegateCombinations.h"
+#include "GameFramework/Actor.h"
 #include "GameplayAbilitySystemComponent.generated.h"
 
-/**
- * 
- */
+class UAbilityAnimNotify;
+class UAnimSequenceBase;
+//class UInteractionAbilityAttributeSet;
+class UDataTable;
+class APawn;
+class AController;
+
+USTRUCT()
+struct FGameplayAttributeApplication
+{
+	GENERATED_BODY()
+
+	// Ability set to grant
+	//UPROPERTY(EditAnywhere)
+	//TSubclassOf<UInteractionAbilityAttributeSet> AttributeSetType;
+
+	// Data table reference to initialize the attributes with, if any (can be left unset)
+	UPROPERTY(EditAnywhere)
+	UDataTable* InitializationData = nullptr;
+};
+
 UCLASS(ClassGroup = AbilitySystem, meta = (BlueprintSpawnableComponent))
 class ABILITY_API UGameplayAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+		TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+		TArray<FGameplayAttributeApplication> DefaultAttributes;
+
+	//~ Begin UAbilitySystemComponent interface
+	virtual void InitAbilityActorInfo(AActor* InOwnerActor, AActor* InAvatarActor) override;
+	//~ End UAbilitySystemComponent interface
+
+	//~ Begin UObject interface
+	virtual void BeginDestroy() override;
+	//~ End UObject interface
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay|Abilities")
+		FGameplayAbilitySpecHandle GrantAbilityOfType(TSubclassOf<UGameplayAbility> AbilityType, bool bRemoveAfterActivation);
+
+	UFUNCTION(BlueprintCallable, Category = "Gameplay|Abilities")
+		void RemoveAbilityOfType(TSubclassOf<UGameplayAbility> AbilityType);
+
+protected:
+	void GrantDefaultAbilitiesAndAttributes();
+
+	UFUNCTION() // UFunction to be able to bind with dynamic delegate
+	void OnPawnControllerChanged(APawn* Pawn, AController* NewController);
+	TArray<FGameplayAbilitySpecHandle> DefaultAbilityHandles;
+
+	UPROPERTY(transient)
+	TArray<UAttributeSet*> AddedAttributes;
 };
