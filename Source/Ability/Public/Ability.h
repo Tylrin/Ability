87555e361058190926d1d8bc2e2722e511ef69e0
Tylrin <t.gellien@outlook.com>
2021-10-27 23:14:00 +0200

    Add ability header

--- Source/Ability/Public/Ability.h
+++ Source/Ability/Public/Ability.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogInteraction, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogAbility, Log, All);
 
 class FAbilityModule : public IModuleInterface
 {
